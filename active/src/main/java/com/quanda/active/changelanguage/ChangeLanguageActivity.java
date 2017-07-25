54c4c5c0eeb610c30a9c2a7d5163cfce4ddb967a
Dang Anh Quan <dang.anh.quan@framgia.com>
2017-07-25 15:32:17 +0700

    multi-language-active

---
+++ active/src/main/java/com/quanda/active/changelanguage/ChangeLanguageActivity.java
@@ -0,0 +1,44 @@
+package com.quanda.active.changelanguage;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import com.quanda.active.Data.ItemClickListener;
+import com.quanda.active.R;
+import com.quanda.active.databinding.ActivityChangeLanguageBinding;
+import com.quanda.active.model.Language;
+import com.quanda.active.utils.LanguageUtils;
+
+public class ChangeLanguageActivity extends AppCompatActivity {
+
+    private LanguageAdapter mLanguageAdapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityChangeLanguageBinding binding =
+                DataBindingUtil.setContentView(this, R.layout.activity_change_language);
+        mLanguageAdapter = new LanguageAdapter(LanguageUtils.getLanguageData());
+        mLanguageAdapter.setListener(new ItemClickListener<Language>() {
+            @Override
+            public void onClickItem(int position, Language language) {
+                if (!language.getCode().equals(LanguageUtils.getCurrentLanguage().getCode())) {
+                    onChangeLanguageSuccessfully(language);
+                }
+            }
+        });
+        LinearLayoutManager layoutManager = new LinearLayoutManager(ChangeLanguageActivity.this);
+        binding.recyclerViewLanguage.setLayoutManager(layoutManager);
+        binding.recyclerViewLanguage.setAdapter(mLanguageAdapter);
+    }
+
+    private void onChangeLanguageSuccessfully(final Language language) {
+        mLanguageAdapter.setCurrentLanguage(language);
+        LanguageUtils.changeLanguage(language);
+        setResult(RESULT_OK, new Intent());
+        finish();
+    }
+}
